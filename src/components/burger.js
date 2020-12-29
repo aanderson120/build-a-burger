53709e383f33c5f12a0676ab0546901daba85561
Amanda Anderson <aanderson_120@yahoo.com>
2020-12-29 12:12:56 -0600

    added lettuce

--- src/components/burger.js
+++ src/components/burger.js
@@ -24,6 +24,18 @@ export default class Burger extends Component {
                 stateValue = lettuce;
                 break;
             }
+            case 'tomato': {
+                stateValue = tomato;
+                break;
+            }
+            case 'cheese': {
+                stateValue = cheese;
+                break;
+            }
+            case 'meat': {
+                stateValue = meat;
+                break;
+            }
             default: break;
         }
         if(action === 'add') {
@@ -36,17 +48,53 @@ export default class Burger extends Component {
         });
     }
 
+    burgerContent = () => {
+        let {
+            lettuce,
+            tomato,
+            cheese,
+            meat
+        } = this.state;
+
+        let burger = [];
+
+        for (let i = 0; i <lettuce; i++) {
+            burger.push(<div key= {burger.length} className="lettuceSide"></div>);
+        }
+        return burger;
+
+    
+
+
+    }
     render() {
         return (
             <>
                 <div className="burgerIngredients">
-
+                    <div className="topSide"></div>
+                    {this.burgerContent()}
+                    <div className ="bottomSide"></div>
                 </div>
                 <div className="ingredientsBlock">
                     <p>Lettuce</p>
                     <div className="ingrBttns">
                     <button onClick={() => this.addRemoveIngredient('add', 'lettuce')} className="ingrBttn">Add</button>
                     <button onClick={() => this.addRemoveIngredient('remove', 'lettuce')} className="ingrBttn">Remove</button>
+                    </div>
+                    <p>Tomato</p>
+                    <div className="ingrBttns">
+                    <button onClick={() => this.addRemoveIngredient('add', 'tomato')} className="ingrBttn">Add</button>
+                    <button onClick={() => this.addRemoveIngredient('remove', 'tomato')} className="ingrBttn">Remove</button>
+                    </div>
+                    <p>Cheese</p>
+                    <div className="ingrBttns">
+                    <button onClick={() => this.addRemoveIngredient('add', 'cheese')} className="ingrBttn">Add</button>
+                    <button onClick={() => this.addRemoveIngredient('remove', 'cheese')} className="ingrBttn">Remove</button>
+                    </div>
+                    <p>Meat</p>
+                    <div className="ingrBttns">
+                    <button onClick={() => this.addRemoveIngredient('add', 'meat')} className="ingrBttn">Add</button>
+                    <button onClick={() => this.addRemoveIngredient('remove', 'meat')} className="ingrBttn">Remove</button>
                     </div>
                 </div>
             </>
