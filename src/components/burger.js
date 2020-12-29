9b17f8128900270c9d6b34dbef750d3db0e98869
Amanda Anderson <aanderson_120@yahoo.com>
2020-12-29 14:09:27 -0600

    added onion/pickle

--- src/components/burger.js
+++ src/components/burger.js
@@ -4,15 +4,19 @@ import './burger.css';
 export default class Burger extends Component {
     state = {
         lettuce: 0,
+        onion: 0,
         tomato: 0,
+        pickle:0,
         cheese: 0,
-        meat: 0
+        meat: 0,
     }
 
     addRemoveIngredient = (action, ingredient) => {
         let {
             lettuce,
+            onion,
             tomato,
+            pickle,
             cheese,
             meat
         } = this.state;
@@ -24,8 +28,16 @@ export default class Burger extends Component {
                 stateValue = lettuce;
                 break;
             }
+            case 'onion': {
+                stateValue = onion;
+                break;
+            }            
             case 'tomato': {
                 stateValue = tomato;
+                break;
+            }
+            case 'pickle': {
+                stateValue = pickle;
                 break;
             }
             case 'cheese': {
@@ -51,7 +63,9 @@ export default class Burger extends Component {
     burgerContent = () => {
         let {
             lettuce,
+            onion,
             tomato,
+            pickle,
             cheese,
             meat
         } = this.state;
@@ -61,8 +75,14 @@ export default class Burger extends Component {
         for (let i = 0; i <lettuce; i++) {
             burger.push(<div key= {burger.length} className="lettuceSide"></div>);
         }
+        for (let i = 0; i <onion; i++) {
+            burger.push(<div key= {burger.length} className="onionSide"></div>);
+        }
         for (let i = 0; i <tomato; i++) {
             burger.push(<div key= {burger.length} className="tomatoSide"></div>);
+        }
+        for (let i = 0; i <pickle; i++) {
+            burger.push(<div key= {burger.length} className="pickleSide"></div>);
         }
         for (let i = 0; i <cheese; i++) {
             burger.push(<div key= {burger.length} className="cheeseSide"></div>);
@@ -90,10 +110,20 @@ export default class Burger extends Component {
                     <button onClick={() => this.addRemoveIngredient('add', 'lettuce')} className="ingrBttn">Add</button>
                     <button onClick={() => this.addRemoveIngredient('remove', 'lettuce')} className="ingrBttn">Remove</button>
                     </div>
+                    <p>Onion</p>
+                    <div className="ingrBttns">
+                    <button onClick={() => this.addRemoveIngredient('add', 'onion')} className="ingrBttn">Add</button>
+                    <button onClick={() => this.addRemoveIngredient('remove', 'onion')} className="ingrBttn">Remove</button>
+                    </div>
                     <p>Tomato</p>
                     <div className="ingrBttns">
                     <button onClick={() => this.addRemoveIngredient('add', 'tomato')} className="ingrBttn">Add</button>
                     <button onClick={() => this.addRemoveIngredient('remove', 'tomato')} className="ingrBttn">Remove</button>
+                    </div>
+                    <p>Pickle</p>
+                    <div className="ingrBttns">
+                    <button onClick={() => this.addRemoveIngredient('add', 'pickle')} className="ingrBttn">Add</button>
+                    <button onClick={() => this.addRemoveIngredient('remove', 'pickle')} className="ingrBttn">Remove</button>
                     </div>
                     <p>Cheese</p>
                     <div className="ingrBttns">
